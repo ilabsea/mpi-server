d5a79b8f10191119fd0feb2ab1cc2804d53bcdd3
Channa Ly <channa.info@gmail.com>
2016-12-12 11:39:49 +0700

    fix layout

--- application/controllers/datas.php
+++ application/controllers/datas.php
@@ -5,7 +5,9 @@ class Datas extends MpiController {
 		$data = array();
 		$data["mpi_patient"] = 1;
 		$data["mpi_visit"] = 1;
-		$this->load->template("templates/general", "datas/csv_export", Iconstant::MPI_APP_NAME, $data);
+		// $this->load->template("templates/general", "datas/csv_export", Iconstant::MPI_APP_NAME, $data);
+    $this->set_view_variables($data);
+    return $this->render_view("csv_export");
 	}
 
 	function downloadcsv() {
@@ -17,9 +19,9 @@ class Datas extends MpiController {
 		   //
 		} else {
 		    die("Failed while creating zip file");
-		    
+
 		}
-		
+
 		if (isset($_POST["mpi_patient"])) :
 			$data = $this->data->get_data_from_table("mpi_patient");
 			$csv_file = FCPATH.APPPATH."tmp/csv_export_patient.csv";
@@ -35,7 +37,7 @@ class Datas extends MpiController {
 			fclose($fp);
 			$zip->addFile($csv_file, "mpi_patient.csv");
 		endif;
-		
+
 		if (isset($_POST["mpi_visit"])) :
 			$data = $this->data->get_data_from_table("mpi_visit");
 			$csv_file = FCPATH.APPPATH."tmp/csv_export_visit.csv";
@@ -52,12 +54,12 @@ class Datas extends MpiController {
 			$zip->addFile($csv_file, "mpi_visit.csv");
 		endif;
 		$zip->close();
-		
+
 		$file_name = "data_".date("YmdHis").".zip";
     	header("Content-disposition: attachment; filename=".$file_name);
 		header("Content-type: text/plain");
 		$content = file_get_contents($zip_file);
-		
+
 		unlink($zip_file);
 		//unlink($csv_file);
 		echo $content;
